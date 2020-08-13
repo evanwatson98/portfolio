48a63c492b1fb1238a66dd4917cd86a7fa6e3bdb
Evan W <evwatson@iu.edu>
2020-08-13 11:03:25 -0700

    Completed motion of icons

--- Portfolio Website/purpose.js
+++ Portfolio Website/purpose.js
@@ -1,65 +1,110 @@
 document.addEventListener("keydown", checkKey);
+
+var move = "";
+
+document.addEventListener('keyup', (event) => {
+		const keyName = event.key;
+		if(keyName == "ArrowRight" || keyName == "ArrowLeft"){
+			clearInterval(move);
+			console.log("Release");
+			console.log("Release Clear Move");
+//			alert('keyup event\n\n' + 'key: ' + keyName);
+
+		}
+	});
+var ogX = document.querySelector("#curSun").getBoundingClientRect().left;
 function checkKey(e) {
+	
 
 //    e = e || window.event;
+	var sunBox = document.querySelector("#curSun");
 	
-	var x, y = 0;
+	var x_velocity = 1;
+	var y_velocity = -1 * Math.sin;
+
+	var position = sunBox.getBoundingClientRect();
+	var x = position.left;
+	var y = position.top;
+	var moveRightx = x +  x_velocity;
+	var moveLeftx =  -  x_velocity;
 	
 	var k = 0;
-	var sunBox = document.querySelector("#curSun");
-	var move = "";
 	
-	var alpha = 20;
+	var alpha = .75;
 	var vel = 10;
 	var deltaSec = .01;
 	
-	var x_velocity = vel * deltaSec * Math.cos(alpha);
-	var y_velocity = vel * deltaSec * Math.sin(alpha);
+//	var x_velocity = vel * deltaSec * Math.cos(alpha);
+//	var y_velocity = vel * deltaSec * Math.sin(alpha);
 	
+	//&& ((sunBox.offsetLeft + x) <= 279)
     if (e.keyCode == '39') {
         // right arrow
 		k = 0;
-		move = setInterval(moveSun,3);
+		clearInterval(move);
+		move = setInterval(startMove, 10);
 		
     }
     else if (e.keyCode == '37') {
        // left arrow
 		k = 1;
-		moveSun();
+		clearInterval(move);
+		move = setInterval(startMove, 10);
     }
 	
-	function moveSun(){
-		e = e || window.event;
-		//move right
-		move = setInterval(startMove, 5);
-		
-	}
+	var boxLimit = 200;
+	var r = 255;
+	var g = 193;
+	var b = 77;
+	var purposeBackground = document.querySelector("#wakeUpContainer");
+	var timeIcon = document.querySelector("#timeIcon");
 	
 	function startMove(){
 		e = e || window.event;
 		
-		var sunRect = sunBox.getBoundingClientRect();
-		x = sunRect.x;
-		y = sunRect.y;
-
-		if(k==0){
+		console.log(sunBox.offsetLeft);
+		
+		if( (sunBox.offsetLeft == boxLimit) && k != 1){
+			console.log("Clear Move");
+			clearInterval(move);
+		}
+		else if(k==0){
 			//move right
-			x = x + x_velocity;
-			y = y + y_velocity;
+			if(sunBox.offsetLeft == ((ogX+x_velocity)*2)){
+				purposeBackground.style.backgroundColor = "rgb(" + r +", " + g +", " + b +")";
+				timeIcon.style.content = "url('sun.svg')";
+			}
+			else if(sunBox.offsetLeft == ((ogX+x_velocity)*3)){
+				purposeBackground.style.backgroundColor = "rgb(" + 0 +", " + 0 +", " + 0 +")";
+				timeIcon.style.content = "url('cresMoon.svg')";
+			}
 			
-			sunBox.style.left = (sunBox.offsetLeft + x) + "px";
-			sunBox.style.top = (sunBox.offsetTop + y) + "px";
-			console.log("x: " + (sunBox.offsetLeft + x) + "px, y: " + (sunBox.offsetTop + y));
+			sunBox.style.left = sunBox.offsetLeft + x_velocity + "px";
+//			sunBox.style.top = (sunBox.offsetTop + y_velocity) + "px";
+//				sunBox.style.top = y + y_velocity;
 		}
 		else if(k==1){
-			//move left
+			
+			if(sunBox.offsetLeft == ((ogX+x_velocity)*2)){
+				purposeBackground.style.backgroundColor = "rgb(" + 36+", " + 197 +", " + 255 +")";
+				timeIcon.style.content = "url('plainSun.svg')";
+			}
+			else if(sunBox.offsetLeft == ((ogX+x_velocity)*3)){
+				purposeBackground.style.backgroundColor = "rgb(" + r +", " + g +", " + b +")";
+				timeIcon.style.content = "url('sun.svg')";
+			}
+			
+			if(sunBox.offsetLeft > ogX){
+				//move left
+				sunBox.style.left = sunBox.offsetLeft - x_velocity + "px";
+	//			sunBox.style.top = (sunBox.offsetTop + (-1*(sunBox.offsetTop * Math.sin(5)))) + "px";
+	//			sunBox.style.top = slope;
+	//			console.log("x: " + moveLeftx + "px, y: " + ( slope) + "px");
+			}else{
+				clearInterval(move);
+			}
 			
 		}
-	}
-	
-	function stopMove(){
-		e = e || window.event;
-		
 	}
 
 }
