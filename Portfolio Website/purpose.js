ea7b1b21e1ad4512ad17d943aebcc8121aa24174
evwatson <evwatson@iu.edu>
2020-08-24 13:33:57 -0500

    adding upward movement to ball

--- Portfolio Website/purpose.js
+++ Portfolio Website/purpose.js
@@ -16,7 +16,7 @@ function checkKey(e) {
 	var sunBox = document.querySelector("#curSun");
 	
 	var x_velocity = 1;
-	var y_velocity = -1 * Math.sin;
+//	var y_velocity = -1 * Math.sin;
 
 	var position = sunBox.getBoundingClientRect();
 	var x = position.left;
@@ -37,79 +37,102 @@ function checkKey(e) {
         // right arrow
 		k = 0;
 		clearInterval(move);
-		move = setInterval(startMove, 10);
+		move = setInterval(startMoveR, 5);
 		
     }
     else if (e.keyCode == '37') {
        // left arrow
 		k = 1;
 		clearInterval(move);
-		move = setInterval(startMove, 10);
+		move = setInterval(startMoveL, 5);
     }
 	
 	var r = 255;
 	var g = 193;
 	var b = 77;
-	var purposeBackground = document.querySelector("#wakeUpContainer");
+//	var purposeBackground = document.querySelector("#wakeUpContainer");
+	var purposeBackgroundPlainSun = document.querySelector("#purposeBackgroundPlainSun");
+	var purposeBackgroundSun = document.querySelector("#purposeBackgroundSun");
+	var purposeBackgroundCresMoon = document.querySelector("#purposeBackgroundCresMoon");
+	
 	var timeIcon = document.querySelector("#timeIcon");
 	var whyWakeBox = document.querySelector("#whyWake");
 	var whyStepBox = document.querySelector("#whyStep");
 	var whyStayBox = document.querySelector("#whyStay");
-	
-	function startMove(){
-		var boxLimit = document.querySelector("#wakeUp").offsetLeft - 80;
-		e = e || window.event;
+	var plainSun = document.querySelector("#plainSun");
+	var sun = document.querySelector("#sun");
+	var cresMoon = document.querySelector("#cresMoon");
+	var y_velocity = 2;
+	var boxLimit = document.querySelector("#wakeUp").offsetLeft - 80;
+	var c = 1;
+
+	function startMoveR(){
+//		e = e || window.event;
 		
-		console.log(sunBox.offsetLeft);
+//		console.log(sunBox.offsetLeft);
 		
-		if( (sunBox.offsetLeft == boxLimit) && k != 1){
-			console.log("Clear Move");
-			clearInterval(move);
-		}
-		else if(k==0){
 			//move right
+				sunBox.style.left = sunBox.offsetLeft + x_velocity + "px";
+				sunBox.style.top = sunBox.offsetTop - y_velocity + "px";
+	//			sunBox.style.top = sunBox.offsetTop - y_velocity + "px";
+	//			
+				whyWakeBox.style.top = whyWakeBox.offsetTop - x_velocity + "px";
+				whyStepBox.style.top = whyStepBox.offsetTop - x_velocity + "px";
+				whyStayBox.style.top = whyStayBox.offsetTop - x_velocity + "px";
+
+			if(sunBox.offsetLeft == ogX-x_velocity){clearInterval(move)};
+			 
 			if(sunBox.offsetLeft == ((ogX+x_velocity)*6)){
-				purposeBackground.style.backgroundColor = "rgb(" + r +", " + g +", " + b +")";
-				timeIcon.style.content = "url('sun.svg')";
+//				purposeBackground.style.backgroundColor = "rgb(" + r +", " + g +", " + b +")";
+				plainSun.style.visibility= "hidden";
+				sun.style.visibility = "visible";
+				purposeBackgroundPlainSun.style.visibility = "hidden";
+				purposeBackgroundSun.style.visibility = "visible";
+//				sunBox.style.left = sunBox.offsetLeft + x_velocity + "px";
 			}
 			else if(sunBox.offsetLeft == ((ogX+x_velocity)*12)){
-				purposeBackground.style.backgroundImage = "url('stars.jpg')";
-				timeIcon.style.content = "url('cresMoon.svg')";
+//				purposeBackground.style.backgroundImage = "url('stars.jpg')";
+				sun.style.visibility= "hidden";
+				cresMoon.style.visibility = "visible";
+				purposeBackgroundSun.style.visibility = "hidden";
+				purposeBackgroundCresMoon.style.visibility = "visible";
+//				sunBox.style.left = sunBox.offsetLeft + x_velocity + "px";
 			}
 			
-			sunBox.style.left = sunBox.offsetLeft + x_velocity + "px";
-//			sunBox.style.top = sunBox.offsetTop - y_velocity + "px";
-//			
-			whyWakeBox.style.top = whyWakeBox.offsetTop - x_velocity + "px";
-			whyStepBox.style.top = whyStepBox.offsetTop - x_velocity + "px";
-			whyStayBox.style.top = whyStayBox.offsetTop - x_velocity + "px";
 		}
-		else if(k==1){
-			
-			if(sunBox.offsetLeft == ((ogX+x_velocity)*6)){
-				purposeBackground.style.backgroundColor = "rgb(" + 36+", " + 197 +", " + 255 +")";
-				timeIcon.style.content = "url('plainSun.svg')";
+	
+	function startMoveL(){
+		if(sunBox.offsetLeft == ((ogX+x_velocity)*6)){
+//				purposeBackground.style.backgroundColor = "rgb(" + 36+", " + 197 +", " + 255 +")";
+				sun.style.visibility= "hidden";
+				plainSun.style.visibility = "visible";
+				purposeBackgroundSun.style.visibility = "hidden";
+				purposeBackgroundPlainSun.style.visibility = "visible";
+//				sunBox.style.left = sunBox.offsetLeft - x_velocity + "px";
 			}
 			else if(sunBox.offsetLeft == ((ogX+x_velocity)*12)){
-				purposeBackground.style.backgroundImage = "none";
-				purposeBackground.style.backgroundColor = "rgb(" + r +", " + g +", " + b +")";
-				timeIcon.style.content = "url('sun.svg')";
+//				purposeBackground.style.backgroundImage = "none";
+//				purposeBackground.style.backgroundColor = "rgb(" + r +", " + g +", " + b +")";
+				cresMoon.style.visibility= "hidden";
+				sun.style.visibility = "visible";
+				purposeBackgroundSun.style.visibility = "visible";
+				purposeBackgroundCresMoon.style.visibility = "hidden";
+//				sunBox.style.left = sunBox.offsetLeft - x_velocity + "px";
 			}
 			
 			if(sunBox.offsetLeft > ogX){
 				//move left
+
 				sunBox.style.left = sunBox.offsetLeft - x_velocity + "px";
+				sunBox.style.top = sunBox.offsetTop + y_velocity + "px";
+	//			sunBox.style.top = sunBox.offsetTop - y_velocity + "px";
+	//			
 				whyWakeBox.style.top = whyWakeBox.offsetTop + x_velocity + "px";
 				whyStepBox.style.top = whyStepBox.offsetTop + x_velocity + "px";
 				whyStayBox.style.top = whyStayBox.offsetTop + x_velocity + "px";
-	//			sunBox.style.top = (sunBox.offsetTop + (-1*(sunBox.offsetTop * Math.sin(5)))) + "px";
-	//			sunBox.style.top = slope;
-	//			console.log("x: " + moveLeftx + "px, y: " + ( slope) + "px");
 			}else{
 				clearInterval(move);
 			}
+	}
 			
 		}
-	}
-
-}
